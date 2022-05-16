fc9ba8034d03bc3c022538acc9390c57bd5e7574
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-05-16 20:48:34 +0300

    01

---
+++ js/01-vars.js
@@ -0,0 +1,32 @@
+/*
+ * - Переменные и типы примитивов
+ * - const и let
+ * - Логирование с методом console.log()
+ * - Уникальность идентификатора
+ */
+
+const age = 10;
+const totalPrice = 200.74; 
+const userName = 'Chelsy';
+const message = 'Добро пожаловать!';
+const isOpen = true;
+const shouldConfirm = false;
+
+console.log(5);
+
+/*
+ * * - Оператор typeof
+ */
+
+const type = typeof isOpen;
+
+console.log(type);
+
+// alert('marina')
+/*
+ * - Как window.alert() блокирует интерпретацию
+ */
+
+// console.log('До');
+// alert('qweqweqwe');
+// console.log('После');
