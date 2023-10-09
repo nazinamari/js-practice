24f8845798d083a878922a356f3d12af05fe94ba
nazinamari <93985053+nazinamari@users.noreply.github.com>
2023-10-09 12:17:00 +0100

    webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import name from './module-1';
+import './scss/common.scss';
+
+console.log(name);
 console.log("Hello World!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'my-bundle.js',
+        filename: 'bundle.js',
     },
     module: {
-    rules: [
-        { test: /\.css$/, use: 'css-loader' },
-        { test: /\.ts$/, use: 'ts-loader' },
-    ],
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],  
     }, 
+    plugins: [new HtmlWebpackPlugin({template: 'src/index.html'})],
     devServer: {
         port: 4444,
         open: true,
