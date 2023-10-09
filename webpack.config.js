81c74edb179bd99c6d3e90705ae4910ae0d9e0c3
nazinamari <93985053+nazinamari@users.noreply.github.com>
2023-10-09 15:12:54 +0100

    done

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
     entry: './src/index.js',
@@ -27,6 +29,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({ template: 'src/index.html' }),
         new MiniCssExtractPlugin({ filename: 'styles.css' }),
+        new CleanWebpackPlugin(),
     ],
     devServer: {
         port: 44444,
