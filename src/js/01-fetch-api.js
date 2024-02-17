9317ab7926094b0b1efa84d2664bedfa0742de04
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-02-17 23:06:50 +0000

    p

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -15,43 +15,18 @@
 
 import '../css/common.css';
 import pokemonCardTpl from '../templates/pokemon-card.hbs';
-import API from './api-service';
-import getRefs from './get-refs';
 
-const refs = getRefs();
+fetch('https://pokeapi.co/api/v2/pokemon/1/')
+.then(response => {
+    return response.json();
+})
+.then(pokemon => {
+    console.log(pokemon);
 
-refs.searchForm.addEventListener('submit', onSearch);
+    // const markup = pokemonCardTpl(pokemon);
 
-function onSearch(e) {
-  e.preventDefault();
+    // console.log(markup);
 
-  const form = e.currentTarget;
-  const searchQuery = form.elements.query.value;
-
-  API.fetchPokemon(searchQuery)
-    .then(renderPokemonCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
-}
-
-function renderPokemonCard(pokemon) {
-  const markup = pokemonCardTpl(pokemon);
-  refs.cardContainer.innerHTML = markup;
-}
-
-function onFetchError(error) {
-  alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
-}
-
-// =========================================
-
-const url = 'https://newsapi.org/v2/everything?q=cars';
-const options = {
-  headers: {
-    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
-  },
-};
-
-fetch(url, options)
-  .then(r => r.json())
-  .then(console.log);
+}).catch(error => {
+    console.log(error);
+});
